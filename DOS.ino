7adc317d39eb053c10947b31d26fcbea72f7316a
epradomor <aeliaspm@gmail.com>
2016-07-06 20:00:21 -0500

    Add files via upload

---
+++ DOS.ino
@@ -0,0 +1,27 @@
+
+#include <SoftwareSerial.h>  //
+SoftwareSerial mySerial(10, 11); // RX, TX //
+
+//Variables
+  int LED=13;
+  int BOTON=7;
+  int DATO=HIGH;
+ 
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BOTON,INPUT);
+  mySerial.begin(9600);
+}
+ 
+void loop() {
+ mySerial.write(digitalRead(BOTON));
+ delay(110);
+ 
+ if(mySerial.available()){
+ DATO= mySerial.read();
+ }
+      if(DATO==HIGH)
+     digitalWrite(LED,HIGH);
+     else
+     digitalWrite(LED,LOW);
+}
